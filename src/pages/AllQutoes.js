1d383fd90779c9d9b4e4bdcc14d4a9e1c31bdeaf
mutotu <muttalip.aydemir@gmail.com>
2022-12-28 08:32:46 -0800

    added ascending functionality

--- src/pages/AllQutoes.js
+++ src/pages/AllQutoes.js
@@ -1,9 +1,9 @@
 import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
-  { id: "p1", author: "LOL", text: "XOXOXOX" },
+  { id: "p1", author: "OL", text: "XOXOXOX" },
   { id: "p2", author: "LOL", text: "XOXOXOX" },
-  { id: "p3", author: "LOL", text: "XOXOXOX" },
+  { id: "p3", author: "XOL", text: "XOXOXOX" },
 ];
 
 const AllQuotes = () => {
