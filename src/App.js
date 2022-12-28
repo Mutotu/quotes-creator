2a2e928d96299ab7d0b77e426428a9dc3f6ed686
mutotu <muttalip.aydemir@gmail.com>
2022-12-27 17:28:12 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQutoes";
+import NewQuotes from "./pages/NewQuotes";
+import QutoeDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes'> </Redirect>
+      </Route>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QutoeDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuotes />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/NewQuotes.js
@@ -0,0 +1,5 @@
+const NewQuotes = () => {
+  return <h1>New Qutoes Page</h1>;
+};
+
+export default NewQuotes;
